225f420459503e76608f9ebc48140df19e9dd2e3
DarkBird <josepokemon234@gmail.com>
2018-10-18 17:14:54 +0200

    Now we have a window, also added an update function with an infinite loop

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print(test)
+from graphics import *
+
+framerate = 10
+scl = 20 #Size of the grid's square
+gridSize = 32 #Amount of squares in the grid
+win = GraphWin("Snake Game", gridSize*scl + 1, gridSize*scl + 1)
+
+def update ():
+    while True:
+        draw()
+        time.sleep(1/framerate)
+
+
+def draw ():
+    for i in range(gridSize):
+        for j in range(gridSize):
+            gridSquare = Rectangle(Point(i*scl, j*scl), Point(i*scl + scl, j*scl + scl))
+            gridSquare.setOutline("white")
+            gridSquare.setFill("black")
+            gridSquare.draw(win)
+    
+
+update()
